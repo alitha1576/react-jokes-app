2a069ad85467db70c28b6652a5615f1eed23911d
Anna Petrova <ann.undine@gmail.com>
2025-04-28 12:17:00 +0200

    add toggle functionality to save button

--- src/components/JokeCard.tsx
+++ src/components/JokeCard.tsx
@@ -16,20 +16,30 @@ export default function JokeCard({
   const [showPunchline, setShowPunchline] = useState(false);
   const [buttonStyle, setButtonStyle] = useState({});
 
+  const [isSaved, setIsSaved] = useState(false);
+
   const handleClick = () => {
     setShowPunchline(!showPunchline);
     setButtonStyle({ display: 'none' });
   };
 
+  const handleSaveButtonClick = () => {
+    setIsSaved(prev => !prev);
+  }
+
+  const SaveButtonStyle = {
+backgroundImage: isSaved ? "url('./src/assets/icons8-bookmark-48-3.png')" : "url('./src/assets/icons8-bookmark-48-2.png')"
+  }
+
   return (
     <div className="card" key={id}>
-      <p className="jokeType">{type}</p>
+      <button className="saveButton" style={SaveButtonStyle} onClick={handleSaveButtonClick}></button>
+      <p className="jokeType">{type.charAt(0).toUpperCase() + type.slice(1)}</p>
       <h2 className="jokeSetup">{setup}</h2>
       {showPunchline && <p className="jokePunchline">{punchline}</p>}
       <button className="showButton" style={buttonStyle} onClick={handleClick}>
         Show
       </button>
-      {/* <button className='saveButton'></button> */}
     </div>
   );
 }
