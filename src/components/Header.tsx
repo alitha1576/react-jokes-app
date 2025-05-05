6b4c5ccfe0207dc9c5da5d8a0a0a768aafb94db7
Anna Petrova <ann.undine@gmail.com>
2025-05-05 10:56:39 +0200

    add tab switching logic to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
+
 export default function Header() {
+  const [activeTab, setActiveTab] = useState('new');
   return (
     <div className="headerContainer">
-      <div className="tab tabNew">New Jokes</div>
-      <div className="tab tabLibrary">Library</div>
+      <div
+        className={`tab ${activeTab === 'new' ? 'tabActive tabActiveRight' : ''}`}
+        onClick={() => setActiveTab('new')}
+      >
+        New Jokes
+      </div>
+      <div
+        className={`tab ${activeTab === 'library' ? 'tabActive tabActiveLeft' : ''}`}
+        onClick={() => setActiveTab('library')}
+      >
+        Library
+      </div>
     </div>
   );
 }
